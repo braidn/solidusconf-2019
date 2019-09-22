bdbe3a4916510e5b2cf65ff7f3349c1887cce97a
Braden Douglass <braden.douglass@gmail.com>
2019-09-22 16:21:36 -0400

    Fix Font Rendering.
    Glossier's Apercu not rendering properly. Some playing here and changes
    to the way that the custom themeing works.

--- theme.js
+++ theme.js
@@ -1,6 +1,9 @@
 export default {
-  // custom colors
-  font: 'Apercu',
+  fonts: {
+    body: 'Apercu, sans-serif',
+    heading: 'Apercu, sans-serif',
+    monospace: '"Apercu Mono", monospace'
+  },
   colors: {
     background: '#f5e3e7',
   },
